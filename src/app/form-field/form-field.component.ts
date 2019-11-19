474b64aab7d9d0bc8d7f2b0a34b44205a9564743
Eweber77 <emmanuel.weber@gmail.com>
2019-11-19 11:31:13 +0100

    3 files modified

--- src/app/form-field/form-field.component.ts
+++ src/app/form-field/form-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form-field',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-field.component.css']
 })
 export class FormFieldComponent implements OnInit {
-
+  Party1 = new FormControl('', [Validators.required]);
+  Party2 = new FormControl('', [Validators.required]);
   constructor() { }
 
   ngOnInit() {
