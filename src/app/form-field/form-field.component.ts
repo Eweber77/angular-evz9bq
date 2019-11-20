13aeb113724600907f65f854066a08d15d823969
Eweber77 <emmanuel.weber@gmail.com>
2019-11-20 09:54:55 +0100

    2 files modified

--- src/app/form-field/form-field.component.ts
+++ src/app/form-field/form-field.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 
+export interface DC {
+  id: string;
+  Value: string;
+}
+
 @Component({
   selector: 'app-form-field',
   templateUrl: './form-field.component.html',
@@ -16,7 +21,16 @@ export class FormFieldComponent implements OnInit {
   Rate = new FormControl('', [Validators.required]);
   constructor() { }
 
+
+
   ngOnInit() {
   }
 
+}
+export class DayCountChoice {
+  DayCounts: DC[] = [
+    {id:"1",Value: 'ACT/360'},
+    {id:"2",Value: 'ACT/365'},
+    {id:"3",Value: '30/360'}  
+  ];
 }
