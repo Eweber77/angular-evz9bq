b10126754363d85bd05845e1aac965fdb412d4ad
Eweber77 <emmanuel.weber@gmail.com>
2019-11-26 11:45:43 +0100

    2 files modified

--- src/app/form-field/form-field.component.ts
+++ src/app/form-field/form-field.component.ts
@@ -2,6 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import { formatDate } from "@angular/common";
 
+export const MY_FORMAT: MatDateFormats = {
+parse: {
+dateInput: 'DD/MM/YYYY',
+},
+display: {
+dateInput: 'DD/MM/YYYY',
+monthYearLabel: 'MMM YYYY',
+dateA11yLabel: 'DD/MM/YYYY',
+monthYearA11yLabel: 'MMMM YYYY',
+},
+};
+
+export var TradeID: number;
+
 
 @Component({
   selector: 'app-form-field',
@@ -12,6 +26,7 @@ import { formatDate } from "@angular/common";
 export class FormFieldComponent implements OnInit {
   TradeID=new FormControl('');
   Party1 = new FormControl('', [Validators.required]);
+  Team = new FormControl('', [Validators.required]);
   Party2 = new FormControl('', [Validators.required]);
   TradeDate = new FormControl('', [Validators.required]);
   MaturityDate = new FormControl('', [Validators.required]);
@@ -28,7 +43,6 @@ export class FormFieldComponent implements OnInit {
     {Value: 'ACT/365'},
     {Value: '30/360'}  ];
 
-
   Currencies: [] = [
     {Value: 'EUR'},
     {Value: 'GBP'},
@@ -39,13 +53,38 @@ export class FormFieldComponent implements OnInit {
   onClickMe() {
     this.clickMessage = "Sending String";
   }
-  setValue(id=string,value: string) {
+  setJSON(id:string,Party1: string,Team:string,Party2: string,TradeDate: Date,StartDate: Date,MaturityDate: Date,Currency: string,Nominal: number,Rate: number,DayCount: string) {
   
+  // Putting 0 as the default value if blank
   if (id==='') 
-    {this.value='0,'+value}
-    else
-    {this.value=id+value}
-}
+    {this.id=0}
+  else 
+    {this.id=id}
+
+  
+  this.message=
+  {
+        "id":this.id,
+        "contractingEntity": Party1,
+        "contractingTeam": "Sales",
+        "counterpart": Party2,
+        "currency": Currency,
+        "dealType": "Loan",
+        "startDate": StartDate,
+        "endDate": MaturityDate,
+        "risk":"",
+        "legs": [
+            {
+                "index": "",
+                "paymentFrequency": "T",
+                "resetFrequency": "",
+                "calculationFrequency": "T",
+                "convention": DayCount,
+                "rateOrMargin": Rate
+            }
+        ]
+  }}
+  
   ngOnInit() {
   }
 
